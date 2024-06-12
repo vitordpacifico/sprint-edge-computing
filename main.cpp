99910283d0d93e2f72123793551d32554e3a1343
Victor Pacifico Dias <160978246+vitordpacifico@users.noreply.github.com>
2024-06-11 22:54:17 -0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -25,13 +25,13 @@ void setup() {
   dht.begin();
 
   lcd.setCursor(0, 0);
-  lcd.print("Inicializando o hardware...");
+  lcd.print("Inicializando...");
   delay(2000);
 }
 
 void loop() {
-  // leitura do sensor DHT22
-  float temperatura = dht.readtemperatura();
+  // leitura do DHT22
+  float temperature = dht.readTemperature();
 
   // leitura do sensor MPU6050
   mpu6050.update();
@@ -40,33 +40,32 @@ void loop() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temperatura: ");
-  lcd.print(temperatura);
-  lcd.print("ºC");
+  lcd.print(temperature);
+  lcd.print("C");
 
-  // print dos valores do acelerômetro e giroscópio
   lcd.setCursor(0, 1);
   lcd.print("AX: ");
-  lcd.print(mpu6050.getAcelX());
+  lcd.print(mpu6050.getAccX());
 
   lcd.setCursor(0, 2);
   lcd.print("AY: ");
-  lcd.print(mpu6050.getAcelY());
+  lcd.print(mpu6050.getAccY());
 
   lcd.setCursor(0, 3);
   lcd.print("AZ: ");
-  lcd.print(mpu6050.getAcelZ());
+  lcd.print(mpu6050.getAccZ());
 
   lcd.setCursor(10, 1);
   lcd.print("GX: ");
-  lcd.print(mpu6050.getGiroX());
+  lcd.print(mpu6050.getGyroX());
 
   lcd.setCursor(10, 2);
   lcd.print("GY: ");
-  lcd.print(mpu6050.getGiroY());
+  lcd.print(mpu6050.getGyroY());
 
   lcd.setCursor(10, 3);
   lcd.print("GZ: ");
-  lcd.print(mpu6050.getGiroZ());
+  lcd.print(mpu6050.getGyroZ());
 
   delay(2000);
 }
